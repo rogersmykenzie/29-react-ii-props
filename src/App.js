ff8efe12fd757f6c316d965027abcb217bc2f3b7
Mykenzie <Work@Mykenzies-MacBook-Pro.local>
2020-03-10 11:06:25 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ShoePreview from "./components/ShoePreview";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      shoes: [{
+        img: "https://stockx-360.imgix.net/Air-Jordan-4-Retro-Bred-2019/Images/Air-Jordan-4-Retro-Bred-2019/Lv2/img01.jpg?auto=format,compress&w=559&q=90&dpr=2&updated_at=1555622524",
+        name: "Jordan Bred 4"
+      }, {
+        img: "https://images-na.ssl-images-amazon.com/images/I/419JoOu471L._AC_.jpg",
+        name: "Moon Shoes"
+      }, {
+        img: "https://media.kohlsimg.com/is/image/kohls/3697022?wid=350&hei=350&op_sharpen=1",
+        name: "Avacrocos"
+      }]
+    }
+  }
+  render() {
+    return (
+      <div>
+        {/* <ShoePreview 
+          shoeImg="https://stockx-360.imgix.net/Air-Jordan-4-Retro-Bred-2019/Images/Air-Jordan-4-Retro-Bred-2019/Lv2/img01.jpg?auto=format,compress&w=559&q=90&dpr=2&updated_at=1555622524" 
+          shoeName="Jordan Bred 4"
+        />
+        <ShoePreview
+          shoeImg="https://images-na.ssl-images-amazon.com/images/I/419JoOu471L._AC_.jpg"
+          shoeName="Moon Shoes"
+        ></ShoePreview>
+        <ShoePreview
+          shoeImg="https://media.kohlsimg.com/is/image/kohls/3697022?wid=350&hei=350&op_sharpen=1"
+          shoeName="Avacrocos"
+        ></ShoePreview> */}
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/ShoePreview.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ShoePreview extends React.Component {
+  render() {
+    console.log(this.props.shoeImg);
+
+
+    return (
+      <div>
+        <img width="100" height="50" src={this.props.shoeImg} alt="Shoes" />
+        <h4>{this.props.shoeName}</h4>
+      </div>
+    );
+  }
+}
+
+export default ShoePreview;
